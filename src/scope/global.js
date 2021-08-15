9c5097c044e478393d92632dad7d762fe6416a71
Efrain Montealegre <efrain.montealegre@gmail.com>
2021-08-15 15:41:48 -0500

    Se terminar explicaciones sobre Scope Global y como no incurrir en malas prácticas

--- src/scope/global.js
+++ src/scope/global.js
@@ -1,7 +1,29 @@
 //Se establecen variables globales
 var hello = "Hello";
+var hello = "Hello  +";
 let world = "Hello World";
-const helloWorld = "Hello World";
-console.log(hello);
+let world = "Hello";
+const helloWorld = "Hello World!";
+
+const anotherFunction = () => {
+    console.log(hello);
+    console.log(world);
+    console.log(helloWorld);
+}
+
+anotherFunction();
 
 
+const helloWorld = () => {
+    globalVar = "im global";
+} 
+
+helloWorld();
+console.log(globalVar);
+
+const anotherFunction = () => {
+    var localVar = globalVar = "Im Global";
+}
+
+anotherFunction();
+console.log(globalVar)
